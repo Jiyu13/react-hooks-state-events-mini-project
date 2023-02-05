4c8fa526a3af11da5be1eb3f1f5533f6675f7db7
Ziru Zou <jiyucoding@yahoo.com>
2023-02-05 02:52:59 -0500

    create handleDeleteItem, filter tasks state & compare, update tasks, pass in CB as prop into Task

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,17 +1,27 @@
 import React from "react";
 import Task from "./Task";
 
-function TaskList({tasks}) {
-  console.log(tasks)
+function TaskList({tasks, setTasks}) {
+
+  function handleDeleteItem(event) {
+    const deletedTaskId = event.target.parentNode.id
+    const newArray = tasks.filter((task) => {
+      return task.text !== deletedTaskId})
+    setTasks(newArray)
+  }
+
+
 
   return (
     <div className="tasks">
 
       {/* display a list of tasks using Task component */}
-
+      
       {/* map each task, return a Task component with */}
       {tasks.map(task => {
-        return <Task key={task.id} task={task} />
+        return <Task key={task.text} text={task.text} category={task.category} onDeleteChange={handleDeleteItem}/>
+        console.log(task)
+        
       })}
 
     </div>
