f8c6f6f42ae8787350c77ed87cea4bfd0874aa1e
Ziru Zou <jiyucoding@yahoo.com>
2023-02-05 01:59:06 -0500

    map each task and display it using Task component with kep property

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,19 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({tasks}) {
+  console.log(tasks)
+
   return (
     <div className="tasks">
+
       {/* display a list of tasks using Task component */}
+
+      {/* map each task, return a Task component with */}
+      {tasks.map(task => {
+        return <Task key={task.id} task={task} />
+      })}
+
     </div>
   );
 }
