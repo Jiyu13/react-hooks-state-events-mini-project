48d292a2efb6e884a4db48b9a4252b639b4496a9
Ziru Zou <jiyucoding@yahoo.com>
2023-02-05 04:46:25 -0500

    add seletedCategoty state, update state in handleFilter, filter tasks by category

--- src/components/App.js
+++ src/components/App.js
@@ -11,14 +11,33 @@ function App() {
 
   const [tasks, setTasks] = useState(TASKS)
 
+  const [selectedCategory, setSelectedCategory] = useState("All")
+  
+  function handleFilter(event) {
+    // console.log(event.target)
+    setSelectedCategory(event.target.id)
+  }
+
+  const tasksToDisplay = tasks.filter(task => {
+    if (selectedCategory === "All") {
+      return task
+    } else {
+      return task.category === selectedCategory
+    }
+  })
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter categories={CATEGORIES}/>
+      <CategoryFilter categories={CATEGORIES} 
+                      onCategoryChange={handleFilter} 
+      />
       <NewTaskForm />
-      <TaskList tasks={tasks} setTasks={setTasks}/>
+      <TaskList tasks={tasksToDisplay} setTasks={setTasks}/>
     </div>
   );
 }
 
 export default App;
+
+
